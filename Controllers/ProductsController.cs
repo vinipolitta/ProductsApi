bcc883122de58124a406d2bb6cfad8a245df5d67
vinipolitta <vinicius.politta1@gmail.com>
2023-09-14 15:15:48 -0300

    Created Controller Product

---
+++ Controllers/ProductsController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ProductsApi.Models;
+
+namespace ProductsApi.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class ProductsController : ControllerBase
+{
+    private static List<Product> products = new List<Product>();
+    private static int id = 0;
+
+    [HttpPost]
+    public IActionResult AddProduct([FromBody] Product product)
+    {
+        product.Id = id++;
+        products.Add(product);
+        return CreatedAtAction(nameof(GetProductsById), new { id = product.Id }, product);
+    }
+
+    [HttpGet]
+    public IEnumerable<Product> RecoveryProducts([FromQuery] int skip = 0, [FromQuery] int take = 5)
+    {
+        return products.Skip(skip).Take(take);
+    }
+
+    [HttpGet("{id}")]
+    public IActionResult GetProductsById(int id)
+    {
+        var product = products.FirstOrDefault(product => product.Id == id);
+        if (product == null) return NotFound("Produto nao encontrado");
+        return Ok(product);
+    }
+
+
+
+}
---
+++ Models/Product.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ProductsApi.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "O Nome do produto e obrigatorio")]
+        [MinLength(4, ErrorMessage = "Deve ter de no minimo 4 caracteres")]
+        public string ProductName { get; set; } = default!;
+
+        [Required(ErrorMessage = "A Descricao do produto e obrigatorio")]
+        public string ProductDescription { get; set; } = default!;
+
+        [Required(ErrorMessage = "A Categoria do produto e obrigatorio")]
+        public string ProductCategory { get; set; } = default!;
+
+        [Required(ErrorMessage = "O preco do produto e obrigatorio")]
+        public float Price { get; set; }
+    }
+}
